8e548ba270f36daf49c4181d54b70db13a23cbb1
Elin Krell <elinkrell1@gmail.com>
2025-12-09 11:33:44 +0100

    Min första commit, lade till readme fil

---
+++ script.sh/projekt.sh
@@ -0,0 +1,50 @@
+#
+# Kan användas för följande attacker:
+# [Skriv möjliga attacker]
+#
+# Author: Frans Schartau
+# Last Update: 2025-01-01
+
+echo "Välkommen till mitt RECON script för att kontrollera en Linux-miljö"
+
+echo
+echo "=== SYSTEMINFO ==="
+uname -a
+
+echo
+echo "=== AKTUELL ANVÄNDARE ==="
+echo $USER
+
+echo
+echo "=== ANVÄNDARE MED SHELL ==="
+grep "sh$" /etc/passwd
+
+echo
+echo "=== NÄTVERK ==="
+ip a | grep inet
+
+echo
+echo "=== LÄGG TILL FLERA TESTER  ==="
+
+#
+# skriv in dina kommandon för tester
+#
+
+lscpu
+free -h
+
+echo "What is your name?"
+read name
+echo "Nice to meet you, $name!"
+
+echo "Choose an option:"
+echo "1) Show current directory"
+echo "2) Show files"
+read choice
+
+if [ "$choice" = "1" ]; then
+	pwd
+else
+	ls
+fi
+
